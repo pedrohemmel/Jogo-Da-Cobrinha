ab29a957a89805dbad26af4197a595fda13a08c3
Pedro Henrique Dias Hemmel de Oliveira <pedro_hemmel@pedro-hemmel>
2021-08-04 12:47:26 -0300

    Ultimo resultado

--- script.js
+++ script.js
@@ -14,19 +14,19 @@ let food = {
 }
 
 function criarBG() {
-    context.fillStyle = 'lightgreen';
+    context.fillStyle = 'black';
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
 function criarCobrinha() {
     for(i = 0; i < snake.length; i++) {
-        context.fillStyle = 'green';
-        context.fillRect(snake[i].x, snake[i].y, box, box)/*cria uma cobra do tamanho da array dele*/
+            context.fillStyle = 'white';
+            context.fillRect(snake[i].x, snake[i].y, box, box)/*cria uma cobra do tamanho da array dele*/
     }
 }
 
 function drawFood() {
-    context.fillStyle = 'red';
+    context.fillStyle = 'rgb(81, 206, 255)';
     context.fillRect(food.x, food.y, box, box);
 }
 
@@ -45,6 +45,13 @@ function iniciarJogo() {
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+    for(i = 1; i < snake.length; i++) {
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            clearInterval(jogo); /*clearInterval vai dar um break no jogo*/
+            alert("Game over");
+        }
+    }
 
     criarBG();
     drawFood()
